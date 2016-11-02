4d45674def6f9a4f4052baa540826a74425cd4d5
Evgeniy Troynov <troinov174@gmail.com>
2016-11-03 02:42:36 +0500

    add user create, update, edit, destroy controller method

--- app/controllers/users.js
+++ app/controllers/users.js
@@ -29,11 +29,67 @@ exports.index = (req, res, next) => {
     });
 };
 
-exports.create  = (req, res, next) => {};
-exports.store   = (req, res, next) => {};
-exports.edit    = (req, res, next) => {};
-exports.update  = (req, res, next) => {};
-exports.destroy = (req, res, next) => {};
+exports.create  = (req, res, next) => {
+   const id = req.params.id || '';
+
+  User
+    .findById(id)
+    .exec((err, user) => {
+      if (err) { return next(err); }
+
+      if (user) { return res.render('dashboard/users/edit', { user: user }); }
+
+      return res.redirect('/dashboard/users');
+    });
+};
+
+exports.store   = (req, res, next) => {
+  const id = req.body.id || '';
+
+  User.create({
+    name: req.body.name,
+    login: req.body.login,
+    lastname: req.body.lastname,
+    telephone: req.body.telephone,
+    password: bcrypt.hashSync(req.body.password, 8),
+  }, (err, user) => {
+    if (err) { return next(err) }
+
+    return res.redirect('/dashboard/users');
+  });
+};
+
+exports.edit    = (req, res, next) => res.render('dashboard/users/edit');
+
+exports.update  = (req, res, next) => {
+  const id = req.body.id || '';
+
+  User.update({
+    _id: id
+  }, {
+    name: req.body.name,
+    login: req.body.login,
+    lastname: req.body.lastname,
+    telephone: req.body.telephone,
+    password: bcrypt.hashSync(req.body.password, 8),
+  }, (err, user) => {
+    if (err) { return next(err) }
+
+    return res.redirect('/dashboard/users');
+  });
+};
+
+exports.destroy = (req, res, next) => {
+  const id = req.params.id || '';
+
+  User
+    .findByIdAndRemove(id)
+    .exec((err) => {
+      if (err) { return next(err); }
+
+      return res.redirect('/dashboard/users');
+    });
+};
 
 exports.login = (req, res) => {
   res.render('user/login');
