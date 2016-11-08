993fb08e572b97eba6e01a2448355fc83b940ceb
Evgeniy Troynov <troinov174@gmail.com>
2016-11-09 01:19:28 +0500

    add colorList, colorListItem components. Move vendor libs in vendor dir

---
+++ public/ts/components/colorListItem.tsx
@@ -0,0 +1,15 @@
+import React, { PropTypes } from 'react';
+
+const ColorItem = ({ className }) => {
+    return (
+
+    );
+};
+
+ColorItem.displayName = 'ColorItem';
+
+ColorItem.propTypes = {
+    className: PropTypes.string,
+};
+
+export default ColorItem;
