864e24ee516e9ea962823f631d1de242285b7a43
Evgeniy Troynov <troinov174@gmail.com>
2016-11-14 02:06:00 +0500

    fix furnityra select

--- public/js/app.js
+++ public/js/app.js
@@ -9,7 +9,6 @@ $(function() {
       $.get('/dashboard/orders/info')
         .done(function(data) {
           data.attributes.forEach(function(attr) {
-            console.info(attr);
             if(attr.group.indexOf(data.attributeGroups[0]._id) !== -1) {
               self.colors.push(attr);
             }
@@ -19,7 +18,7 @@ $(function() {
             }
 
             if(attr.group.indexOf(data.attributeGroups[2]._id) !== -1) {
-              self.glasses.push(attr);
+              self.furnitura.push(attr);
             }
           });
         });
