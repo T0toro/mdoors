1ea844744de9ff7b39da168e20f8c081d033ef4e
Evgeniy Troynov <troinov174@gmail.com>
2016-11-15 13:40:53 +0500

    add tinymce editor

--- public/js/app.js
+++ public/js/app.js
@@ -1,8 +1,24 @@
 'use strict';
 
 $(function() {
+
+  // Tiny mce
+  tinymce.init({
+    selector: '.editor',
+    height: 300,
+    theme: 'modern',
+    plugins: [
+      'advlist autolink lists link image charmap print preview hr anchor pagebreak',
+      'searchreplace wordcount visualblocks visualchars code fullscreen',
+      'insertdatetime media nonbreaking save table contextmenu directionality',
+      'emoticons template paste textcolor colorpicker textpattern imagetools codesample'
+    ],
+    toolbar1: 'insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image',
+    toolbar2: 'print preview media | forecolor backcolor emoticons | codesample',
+  });
+
   new Vue({
-    el: '#page',
+    el: '.page.page-order',
     mounted: function() {
       var self = this;
 
