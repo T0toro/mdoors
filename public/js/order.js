3541f1ff1ffff65de9b861fa62f6af323e2a9097
Evgeniy Troynov <troinov174@gmail.com>
2017-01-16 06:09:58 +0000

    add colors in arka form

--- public/js/order.js
+++ public/js/order.js
@@ -32,9 +32,9 @@ $(function() {
               if (attr.product.indexOf(self.product) !== -1) { self.colorsActive.push(attr); }
             }
 
-            if (data.attributeGroups[0] && attr.group.indexOf(data.attributeGroups[0]._id) !== -1 && (data.products[5] && attr.group.indexOf(data.products[5]._id) !== -1)) {
+            if (data.attributeGroups[0] && attr.group.indexOf(data.attributeGroups[0]._id) !== -1 && (data.products[5] && attr.product.indexOf(data.products[5]._id) !== -1)) {
+              console.info('test', attr);
               self.arkaColors.push(attr);
-              if (attr.product.indexOf(self.product) !== -1) { self.colorsActive.push(attr); }
             }
 
 
