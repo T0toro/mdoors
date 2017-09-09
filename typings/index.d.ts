1c8bea29f22e4d1195e43d67675014b637df603e
Evgeniy Troynov <troinof@gmail.com>
2017-09-10 00:22:41 +0500

    #master. replace typings native npm @types

--- typings/index.d.ts
+++
@@ -1,6 +0,0 @@
-/// <reference path="globals/jquery/index.d.ts" />
-/// <reference path="globals/react-dom/index.d.ts" />
-/// <reference path="globals/react/index.d.ts" />
-/// <reference path="globals/redux/index.d.ts" />
-/// <reference path="globals/select2/index.d.ts" />
-/// <reference path="modules/react-redux/index.d.ts" />
